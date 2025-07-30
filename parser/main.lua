6223d2c2c0cdd82b5859cad80931ad9805321354
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-07-30 23:11:46 +0100

    Parser filtergc fix and add Metadata API

--- parser/main.lua
+++ parser/main.lua
@@ -6,14 +6,30 @@ local exclusions = require("exclusions")
 local extractIndex = require("indexExtractor")
 local extractDoc = require("docExtractor")
 
+local function printf(str, ...)
+    p(string.format(str, ...)) ---@diagnostic disable-line: undefined-global
+end
+
 local tree = {}
 for name, t in fs.scandirSync("../docs") do
     if t == "directory" and not exclusions:contains(name) then
         local library = {}
 
         for funcName, fType in fs.scandirSync("../docs/" .. name) do
+            local formatted = ("../docs/%s/%s"):format(name, funcName)
             if fType ~= "directory" then
-                library[funcName:gsub(".md", "")] = fs.readFileSync(("../docs/%s/%s"):format(name, funcName))
+                library[funcName:gsub(".md", "")] = fs.readFileSync(formatted)
+            else
+                local exists, err = fs.readFileSync(formatted .. "/README.md")
+                if not exists then
+                    printf(
+                        "assumed that directory %s inside of %s (%s/%s) was a function doc with a README.md, but none was found: %s",
+                        funcName, name, name, funcName, err)
+                else
+                    -- filtergc is the only func which uses an entire directory so we are safe (for now),
+                    -- until another big function comes around and requires its own directory
+                    library[funcName] = fs.readFileSync(formatted .. "/README.md")
+                end
             end
         end
 
@@ -52,14 +68,13 @@ functions to be cautious of (due to unique doc format) when parsing:
 -- messy solutions
 request -- done: added compatibility in doc extractor
 getgc -- done: same above
+filtergc -- done: same above + initial documentation scanner which collected markdown files
+-- now recognises when funcs are an entire directory (assuming README.md exists) instead of just one file
 
-filtergc
-
+-- TODO:
 Signals/Connection
 WebSocket library
-
 remove things like annotations, e.g. "-- (1)" or maybe even parse them?!
-
 Drawing lib functions except dont count readme
 
 uhh more i forgor
@@ -94,14 +109,16 @@ for libname, library in pairs(tree) do
     end
 end
 
-fs.mkdirpSync("../docs/built-info")
+fs.mkdirpSync("../docs/built-info") -- old directory, now deprecated
+fs.mkdirpSync("../docs/api")
 
 local bot_json = json.encode(bot, { indent = true })
-fs.writeFileSync("../docs/built-info/bot.json", bot_json)
+fs.writeFileSync("../docs/built-info/bot.json", bot_json) -- old
+fs.writeFileSync("../docs/api/full.json", bot_json)
 
 local viewer = {}
 
-for libname, lib in pairs(bot) do
+for _, lib in pairs(bot) do
     for k, func in pairs(lib) do
         if k ~= "_description" and k ~= "_link" then
             viewer[func.title or ""] = clean(func.description or "")
@@ -112,4 +129,28 @@ end
 viewer[""] = nil
 
 local viewer_json = json.encode(viewer, { indent = true })
-fs.writeFileSync("../docs/built-info/viewer.json", viewer_json)
+fs.writeFileSync("../docs/built-info/viewer.json", viewer_json) -- old
+fs.writeFileSync("../docs/api/mini.json", viewer_json)
+
+
+
+
+-- build the api page
+print("Building API page...")
+
+local http = require("coro-http")
+require("string-extensions") -- Richy-Z/string-extensions
+
+local res, body = http.request("GET", "https://sunc.su/_documentationAPI-preprocess.txt")
+if not (res and res.code == 200 and body) then
+    p("FAILED to retrieveAPI homepage!!!")
+    os.exit(1)
+end
+
+local analytics = fs.readFileSync("../overrides/partials/integrations/analytics/custom.html")
+---@diagnostic disable-next-line: undefined-field
+body = body:gsub(string.deregexify("<numelon-plstrk></numelon-plstrk>"), analytics)
+
+fs.writeFileSync("../docs/api/index.html", body)
+
+print("FINISHED EVERYTHING YIPPEEE")
